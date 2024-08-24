9cf4d2c7b6d13aa71564910bc230ff01f062a881
shivaacodes <shivasajay007@gmail.com>
2024-08-24 21:23:46 +0530

    card component in messfees

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import Dashboard from "./components/Dashboard";
+import BillCard from "./pages/Messfees";
 
 function App() {
   return (
-    <div className="App">
-      <div className="flex justify-between items-center">
-        <Sidebar className="div" />
-        <Navbar className="div" />
+    <div>
+      {" "}
+      {/*className="App flex h-screen">
+      <Sidebar className="w-1/4" />
+      <div className="flex-1 flex flex-col">
+        <Navbar />
+        <Dashboard />
+      </div>*/}
+      <div className="flex justify-end p-4 h-screen">
+        <div className="w-1/3">
+          <BillCard />
+        </div>
       </div>
-      <Dashboard />
     </div>
   );
 }
---
+++ src/pages/Messfees.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+export default function BillCard() {
+  return (
+    <div className="bg-gray-100 text-black rounded-2xl p-6 w-full h-full border-2 border-black">
+      <h2 className="text-2xl text-center font-bold mb-4">Fee Details</h2>
+      <div className="space-y-4">
+        <div className="flex justify-between">
+          <span>ysysysyss</span>
+          <span>244242</span>
+        </div>
+        <div className="flex justify-between">
+          <span>ysyys</span>
+          <span>2662</span>
+        </div>
+        <div className="flex justify-between">
+          <span>ysyys</span>
+          <span>2662</span>
+        </div>
+        <div className="flex justify-between">
+          <span>ysyys</span>
+          <span>2662</span>
+        </div>
+        <div className="flex justify-between">
+          <span>ysyys</span>
+          <span>2662</span>
+        </div>
+        <div className="flex justify-between">
+          <span>ysyys</span>
+          <span>2662</span>
+        </div>
+        <div className="flex justify-between">
+          <span>ysyys</span>
+          <span>2662</span>
+        </div>
+
+        <div className="flex justify-between">
+          <span>ysyys</span>
+          <span>2662</span>
+        </div>
+
+        <div className="flex justify-between">
+          <span>ysyys</span>
+          <span>2662</span>
+        </div>
+        <div className="flex justify-between">
+          <span>ysyys</span>
+          <span>2662</span>
+        </div>
+        <div className="flex justify-between">
+          <span>ysyys</span>
+          <span>2662</span>
+        </div>
+        <div className="flex justify-between">
+          <span>ysyys</span>
+          <span>2662</span>
+        </div>
+
+        <hr className="my-4" />
+
+        <div className="flex justify-between text-xl font-bold">
+          <span>Total: </span>
+          <span>26626262626</span>
+        </div>
+      </div>
+    </div>
+  );
+}
